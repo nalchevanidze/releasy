b0c752e4ad2860011a80f1bb7d44c99baaddb4a7
Daviti Nalchevanidze <d.nalchevanidze@gmail.com>
2026-01-17 16:34:28 +0100

    createLabel

--- packages/core/src/lib/labels/index.ts
+++ packages/core/src/lib/labels/index.ts
@@ -1,11 +1,11 @@
-import {  Config, LabelType } from "../config";
+import { Config, LabelType } from "../config";
 import { Label, parseLabel } from "./parse";
-export { Label, parseLabel } from "./parse";
+export { Label, parseLabel, createLabel } from "./parse";
 
 export const parseLabels = <T extends LabelType>(
   config: Config,
   target: T,
-  labels: string[]
+  labels: string[],
 ): Array<keyof Config[T]> =>
   labels
     .map((label) => parseLabel(config, label))
