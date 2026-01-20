d291bba6136d2a263fd21c86ede699be093fe23d
Daviti Nalchevanidze <d.nalchevanidze@gmail.com>
2026-01-20 09:44:10 +0100

    bump

--- packages/core/src/lib/project/custom.ts
+++ packages/core/src/lib/project/custom.ts
@@ -8,13 +8,10 @@ export class CustomModule implements Module {
 
   public version = () => Version.parse(exec(this.config.version).trim());
 
-  public bump = async (option: BumpType) => {
-    return execVoid(this.config.bump.replace("{{BUMP}}", option));
-  };
+  public bump = (option: BumpType) =>
+    execVoid(this.config.bump.replace("{{BUMP}}", option));
 
-  public postBump = async () => {
-    await execVoid(this.config.postBump);
-  };
+  public postBump = () => execVoid(this.config.postBump);
 
   public pkg = (id: string): string => this.config.pkg.replace("{{PKG}}", id);
 }
