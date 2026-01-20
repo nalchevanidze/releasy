5834ab5189271a1277588c20a43671b8e1d83806
Daviti Nalchevanidze <d.nalchevanidze@gmail.com>
2026-01-20 09:43:00 +0100

    custom

--- packages/core/src/lib/project/custom.ts
+++ packages/core/src/lib/project/custom.ts
@@ -1,4 +1,4 @@
-import { CustomManager, Manager } from "../config";
+import { CustomManager } from "../config";
 import { exec, execVoid } from "../utils";
 import { Version } from "../version";
 import { Module, BumpType } from "./types";
@@ -6,7 +6,7 @@ import { Module, BumpType } from "./types";
 export class CustomModule implements Module {
   constructor(private config: CustomManager) {}
 
-  public version = () => Version.parse(this.config.version);
+  public version = () => Version.parse(exec(this.config.version).trim());
 
   public bump = async (option: BumpType) => {
     return execVoid(this.config.bump.replace("{{BUMP}}", option));
